0a6cad77701e4e325f8d7e03e5d7f4d8cdca4aea
ericrius1 <ericrius1>
2016-04-26 14:58:05 -0700

    init

---
+++ singleSpawner.js
@@ -0,0 +1,21 @@
+//
+//
+//  Created by The Content Team 4/10/216
+//  Copyright 2016 High Fidelity, Inc.
+//
+//
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+
+var orientation = MyAvatar.orientation;
+orientation = Quat.safeEulerAngles(orientation);
+orientation.x = 0;
+orientation = Quat.fromVec3Degrees(orientation);
+var center = Vec3.sum(MyAvatar.getHeadPosition(), Vec3.multiply(2, Quat.getFront(orientation)));
+
+var whiteboardPath = 'https://hifi-public.s3.amazonaws.com/eric/whiteboard/wrapper.js';
+Script.include(whiteboardPath);
+
+var whiteboard = new Whiteboard(center);
