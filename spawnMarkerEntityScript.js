ab1d8b82ddd3498d0b7659af83538318183e9667
ericrius1 <ericrius1>
2016-04-29 15:45:52 -0700

    whiteboard

--- spawnMarkerEntityScript.js
+++ spawnMarkerEntityScript.js
@@ -1,7 +1,8 @@
 (function() {
 
     var _this;
-    var SCRIPT_URL = Script.resolvePath("markerEntityScript.js");
+    var MARKER_SCRIPT_URL = Script.resolvePath("markerEntityScript.js");
+    var ERASER_SCRIPT_URL = Script.resolvePath("eraserEntityScript.js");
     MarkerSpawnerButton = function() {
         _this = this;
         _this.markers = [];
@@ -18,11 +19,83 @@
         startNearTrigger: function() {
             print("CLICK")
             _this.spawnMarkers();
+            _this.spawnEraser();
         },
 
         startFarTrigger: function() {
             print("CLICK")
             _this.spawnMarkers();
+            _this.spawnEraser();
+        },
+
+        spawnEraser: function() {
+            var ERASER_MODEL_URL = "http://hifi-content.s3.amazonaws.com/alan/dev/eraser-2.fbx";
+            var props = Entities.getEntityProperties(_this.entityID);
+            var rightDir = Quat.getRight(props.rotation);
+            var frontDir = Quat.getFront(props.rotation);
+            var eraserPosition = Vec3.sum(props.position, {
+                x: 0,
+                y: -0.8,
+                z: 0.0
+            });
+            eraserPosition = Vec3.sum(eraserPosition, Vec3.multiply(rightDir, -0.5));
+            eraserPosition = Vec3.sum(eraserPosition, Vec3.multiply(frontDir, -0.08));
+            var eraserRotation = Quat.fromVec3Degrees({
+                x: props.rotation.x + 10,
+                y: props.rotation.y - 90,
+                z: props.rotation.z
+            });
+
+            _this.eraser = Entities.addEntity({
+                type: "Model",
+                modelURL: ERASER_MODEL_URL,
+                shapeType: 'box',
+                dynamic: true,
+                position: eraserPosition,
+                rotation: eraserRotation,
+                script: ERASER_SCRIPT_URL,
+                dimensions: {
+                    x: 0.0858,
+                    y: 0.0393,
+                    z: 0.2083
+                },
+                gravity: {
+                    x: 0,
+                    y: -5,
+                    z: 0
+                },
+                velocity: {
+                    x: 0,
+                    y: -0.1,
+                    z: 0
+                },
+                userData: JSON.stringify({
+                    wearable: {
+                        joints: {
+                            RightHand: [{
+                                x: 0.020,
+                                y: 0.120,
+                                z: 0.049
+                            }, {
+                                x: 0.1004,
+                                y: 0.6424,
+                                z: 0.717,
+                                w: 0.250
+                            }],
+                            LeftHand: [{
+                                x: -0.005,
+                                y: 0.1101,
+                                z: 0.053
+                            }, {
+                                x: 0.723,
+                                y: 0.289,
+                                z: 0.142,
+                                w: 0.610
+                            }]
+                        }
+                    }
+                })
+            })
         },
 
         spawnMarkers: function() {
@@ -89,13 +162,13 @@
                     y: 0.027,
                     z: 0.164
                 },
-                script: SCRIPT_URL
+                script: MARKER_SCRIPT_URL
             };
 
             userDataProps.markerColor = {
                 red: 10,
                 green: 10,
-                blue: 200
+                blue: 10
             }
             markerProps.modelURL = _this.markerModelURLS[0];
             markerProps.userData = JSON.stringify(userDataProps);
@@ -106,7 +179,7 @@
             userDataProps.markerColor = {
                 red: 10,
                 green: 10,
-                blue: 10
+                blue: 200
             }
             markerProps.userData = JSON.stringify(userDataProps);
             markerProps.position = markerPosition;
@@ -116,9 +189,9 @@
             markerProps.position = markerPosition;
             markerProps.modelURL = _this.markerModelURLS[2];
             userDataProps.markerColor = {
-                red: 10,
+                red: 200,
                 green: 10,
-                blue: 200
+                blue: 10
             }
             markerProps.userData = JSON.stringify(userDataProps);
             _this.markers.push(Entities.addEntity(markerProps));
@@ -132,6 +205,7 @@
             _this.markers.forEach(function(marker) {
                 Entities.deleteEntity(marker);
             });
+            Entities.deleteEntity(_this.eraser);
         }
 
 
